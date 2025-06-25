b486280e49187a02c76979c9ba764c11e8452aea
RyanBUTINFO <adiyamanbizirin@gmail.com>
2025-06-25 23:12:51 +0200

    Amélioration de la page "voir profil"

--- Views/view_profile.php
+++ Views/view_profile.php
@@ -5,47 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil Utilisateur</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="src/css/profile.css">
+    <link rel="stylesheet" href="src/css/profile.css?v=<?php echo time(); ?>">
 </head>
 <body>
-<div class="page-content page-container" id="page-content">
-    <div class="padding">
-        <div class="row container d-flex justify-content-center">
-            <div class="col-xl-6 col-md-12">
-                <div class="card user-card-full">
-                    <div class="row m-l-0 m-r-0">
-                        <div class="col-sm-4 bg-c-lite-green user-profile" >
-                            <button >
-                                <a href="?controller=chat"class="bg-c-lite-green" id="retour">Retour au chat</a>
-                                <div class="arrow-wrapper">
-                                    <div class="arrow"></div>
-                                </div>
-                            </button>
-                            <div class="card-block text-center text-white">
-                                <div class="m-b-25">
-                                    <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image">
-                                </div>
-                                <h6 class="f-w-600"> Informations</h6>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-block">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Prénom</p>
-                                        <h6 class="text-muted f-w-400"><?=$username?></h6>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Email</p>
-                                        <h6 class="text-muted f-w-400"><?=$mail?></h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<div class="profile-bg">
+    <div class="profile-container">
+        <div class="profile-sidebar">
+            <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="profile-avatar" alt="User-Profile-Image">
+            <h2 class="profile-name"><?=$username?></h2>
+            <div class="profile-info-title">Informations</div>
+            <a href="?controller=chat" class="profile-back-btn">
+                <i class="fas fa-arrow-left"></i> Retour au chat
+            </a>
+        </div>
+        <div class="profile-content">
+            <div class="profile-row">
+                <div class="profile-label">Prénom</div>
+                <div class="profile-value"><?=$username?></div>
+            </div>
+            <div class="profile-row">
+                <div class="profile-label">Email</div>
+                <div class="profile-value"><?=$mail?></div>
             </div>
         </div>
     </div>
